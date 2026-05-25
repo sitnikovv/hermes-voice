cef0aa2cd0845c4027e7402bf3d8fce0ec0820f9
Vyacheslav Sitnikov <vsitnikov@gmail.com>
2026-05-25 07:45:33 +0300

    feat: dispatch fast backend responses

--- internal/dispatch/dispatcher_test.go
+++ internal/dispatch/dispatcher_test.go
@@ -2,6 +2,7 @@ package dispatch
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
@@ -48,9 +49,32 @@ func (a immediateAdapter) Invoke(context.Context, backend.Request) (*backend.Res
 	return a.resp, a.err
 }
 
-type spyRunner struct{}
+type blockingAdapter struct {
+	started chan struct{}
+	release chan struct{}
+	resp    *backend.Response
+	err     error
+}
 
-func (spyRunner) Start(context.Context, Task) {}
+func (a blockingAdapter) Invoke(ctx context.Context, req backend.Request) (*backend.Response, error) {
+	close(a.started)
+	select {
+	case <-a.release:
+		return a.resp, a.err
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+type spyRunner struct {
+	started chan Task
+}
+
+func (r spyRunner) Start(ctx context.Context, task Task) {
+	if r.started != nil {
+		r.started <- task
+	}
+}
 
 func validRequest() backend.Request {
 	return backend.Request{
@@ -61,5 +85,72 @@ func validRequest() backend.Request {
 		ModelID:   "model",
 		BackendID: "backend",
 		ModelName: "test-model",
+	}
+}
+
+func TestInvokeReturnsFastResponseUnchangedAndDoesNotStartRunner(t *testing.T) {
+	want := &backend.Response{ID: "resp-1", Status: backend.StatusCompleted, Output: "done", Metadata: map[string]string{"k": "v"}}
+	runner := spyRunner{started: make(chan Task, 1)}
+	d, err := New(Config{Backend: immediateAdapter{resp: want}, Runner: runner, QuickTimeout: time.Second})
+	if err != nil {
+		t.Fatalf("New() error = %v", err)
+	}
+	got, err := d.Invoke(context.Background(), validRequest())
+	if err != nil {
+		t.Fatalf("Invoke() error = %v", err)
+	}
+	if got != want {
+		t.Fatalf("Invoke() response pointer changed: got %p want %p", got, want)
+	}
+	select {
+	case task := <-runner.started:
+		t.Fatalf("runner started on fast success: %+v", task)
+	default:
+	}
+}
+
+func TestInvokeReturnsFastErrorUnchangedAndDoesNotStartRunner(t *testing.T) {
+	wantErr := errors.New("boom")
+	runner := spyRunner{started: make(chan Task, 1)}
+	d, err := New(Config{Backend: immediateAdapter{err: wantErr}, Runner: runner, QuickTimeout: time.Second})
+	if err != nil {
+		t.Fatalf("New() error = %v", err)
+	}
+	got, err := d.Invoke(context.Background(), validRequest())
+	if !errors.Is(err, wantErr) {
+		t.Fatalf("Invoke() error = %v, want %v", err, wantErr)
+	}
+	if got != nil {
+		t.Fatalf("Invoke() response = %+v, want nil", got)
+	}
+	select {
+	case task := <-runner.started:
+		t.Fatalf("runner started on fast error: %+v", task)
+	default:
+	}
+}
+
+func TestInvokeParentCanceledBeforeTimeoutReturnsContextErrorNoRunner(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	adapter := blockingAdapter{started: make(chan struct{}), release: make(chan struct{})}
+	runner := spyRunner{started: make(chan Task, 1)}
+	d, err := New(Config{Backend: adapter, Runner: runner, QuickTimeout: time.Hour})
+	if err != nil {
+		t.Fatalf("New() error = %v", err)
+	}
+	result := make(chan error, 1)
+	go func() {
+		_, err := d.Invoke(ctx, validRequest())
+		result <- err
+	}()
+	<-adapter.started
+	cancel()
+	if err := <-result; !errors.Is(err, context.Canceled) {
+		t.Fatalf("Invoke() error = %v, want context canceled", err)
+	}
+	select {
+	case task := <-runner.started:
+		t.Fatalf("runner started on parent cancel: %+v", task)
+	default:
 	}
 }
